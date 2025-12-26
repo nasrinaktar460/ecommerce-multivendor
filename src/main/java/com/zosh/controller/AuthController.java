3b6ba7c8ef54b071a6ea308fbf53811d6ff46ad6
Nasrin <nasrinaktar.snt@gmail.com>
2025-12-26 14:06:29 +0530

    Create a user and fixed auth signup controller

--- src/main/java/com/zosh/controller/AuthController.java
+++ src/main/java/com/zosh/controller/AuthController.java
@@ -2,22 +2,27 @@ package com.zosh.controller;
 
 
 import com.zosh.modal.User;
+import com.zosh.repository.UserRepository;
 import com.zosh.response.SignupRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+//@RestController("/auth")
 public class AuthController {
-    @PostMapping
+
+    private final UserRepository userRepository;
+    @PostMapping("/signup")
         public ResponseEntity<User> createUserHandler(@RequestBody SignupRequest req){
             User user = new User();
             user.setEmail(req.getEmail());
             user.setFullName(req.getFullName());
 
-            return ResponseEntity.ok(user);
+            User savedUser=userRepository.save(user);
+            return ResponseEntity.ok(savedUser);
         }
 
 }
